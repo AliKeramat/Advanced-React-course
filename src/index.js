71175468fdd75d940478e4cb98e29b18b4294477
Ali Keramat <alikeramatdev@gmail.com>
2023-01-05 16:35:02 +0330

    Part 3, a little styling and use of States

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.css";
 
 class Hellow extends React.Component {
   render() {
-    return (
-      <h1>Hello my friends</h1>
-    )
+    return <h1>Hello my friends</h1>;
   }
 }
 
 class Timer extends React.Component {
-  render(){
-    return(
-      <h2>it is {new Date().toLocaleTimeString()}</h2>
-    )
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
+  }
+  render() {
+    setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+    return <h2 className="timer">it is {this.state.time} </h2>;
   }
 }
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-      <Hellow/>
-      <Timer/>
+      <div className="main">
+        <Hellow />
+        <Timer />
       </div>
     );
   }
 }
 
-const tick = () => {
-  ReactDOM.render(<App/>, document.getElementById("root"));
-};
-
-setInterval(() => {
-  tick();
-}, 1000);
+ReactDOM.render(<App />, document.getElementById("root"));
